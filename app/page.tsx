59ca7db12e9cb43ca80b7acd2757710b793f4f49
sanek5648 <sashakopyov5@gmail.com>
2026-02-17 10:02:04 +0500

    little changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { authClient } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
 import {
   Code,
-  Flame,
+  Smile,
   Trophy,
   Users,
   BookOpen,
@@ -20,7 +20,6 @@ import {
   ShieldCheck,
   Sun,
   Moon,
-  Sparkles,
 } from "lucide-react";
 
 export default function Home() {
@@ -78,11 +77,11 @@ export default function Home() {
       color: "bg-sky-500",
     },
     {
-      icon: <Flame className="w-8 h-8" />,
+      icon: <Smile className="w-8 h-8" />,
       title: "Сообщество",
       description:
         "Тысячи довольных учеников уже изменили свою жизнь вместе с нами",
-      color: "bg-orange-500",
+      color: "bg-blue-400",
     },
   ];
 
