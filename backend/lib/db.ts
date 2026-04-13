6331f1abb72fa2c3ecfed2392bf62fc89744a380
Nilmkey <bbsjjs41@gmail.com>
2026-04-13 13:49:06 +0500

    add profile page

--- backend/lib/db.ts
+++ backend/lib/db.ts
@@ -3,14 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-<<<<<<< HEAD
-if (!process.env.DB_URL) {
-  console.error("Нет DB_URL в .env");
-}
-
-const client = new MongoClient(process.env.DB_URL!);
-export const db = client.db();
-=======
 const DB_URL = process.env.DB_URL;
 
 if (!DB_URL) {
@@ -19,4 +11,3 @@ if (!DB_URL) {
 
 const client = new MongoClient(DB_URL!);
 export const db: Db = client.db();
->>>>>>> 761e16e7f650bc093d7f7dd1f9e44b6cb70157ef
